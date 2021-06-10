5d146a798e2b19a6af2157f4baaa5ac6e250cd75
unknown <jainesirius16@gmail.com>
2021-06-10 14:19:24 -0300

    Alertas Jaine

---
+++ src/app/delete/tema-delete/tema-delete.component.ts
@@ -0,0 +1,50 @@
+import { environment } from './../../../environments/environment.prod';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TemaService } from './../../service/tema.service';
+import { Component, OnInit } from '@angular/core';
+import { Tema } from 'src/app/model/Tema';
+import { AlertasService } from 'src/app/service/alertas.service';
+
+@Component({
+  selector: 'app-tema-delete',
+  templateUrl: './tema-delete.component.html',
+  styleUrls: ['./tema-delete.component.css']
+})
+export class TemaDeleteComponent implements OnInit {
+
+  tema: Tema = new Tema()
+  idTema: number
+
+  constructor(
+    private temaService: TemaService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private alertas: AlertasService
+  ) { }
+
+  ngOnInit() {
+    if(environment.token == ''){
+      alert ('Sessão expirou')
+      this.router.navigate(['/inicio'])
+    }
+
+    this.idTema = this.route.snapshot.params['id']
+  this.findByIdTema(this.idTema)
+  }
+
+  findByIdTema(id: number) {
+    this.temaService.getByIdTema(id).subscribe((resp: Tema) => {
+      this.tema = resp
+    })
+  }
+
+  delete() {
+    this.temaService.deleteTema(this.idTema).subscribe(() => {
+      this.alertas.showAlertSucess('Tema apagado com sucesso!')
+      this.router.navigate(['/tema'])
+    })
+  }
+
+}
+
+
